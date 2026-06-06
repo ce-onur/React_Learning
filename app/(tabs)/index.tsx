0338db88dc2bf04851456ad4bdfa117043a7e52c
Onur <worknscience@gmail.com>
2026-06-06 18:18:59 +0300

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,26 +6,18 @@ import { Text } from "react-native";
 export default function index() {
   return (
     <ScreenWrapper>
-      <Text className="text-xl text-blue-500 font-bold">
-        Welcome to NativeWind!
-      </Text>
+      <Text className="text-5xl text-blue-500 font-bold">Home </Text>
       <Link
         href={"/(auth)/sign-in"}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         Go to Sign In
       </Link>
       <Link
         href={"/(auth)/sign-up"}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         Go to Sign Up
-      </Link>
-      <Link href={"/subscriptions/spotify"}>Spotify Subscription</Link>
-      <Link
-        href={{ pathname: "/Subscriptions/[id]", params: { id: "claude" } }}
-      >
-        Claude Max Subscription
       </Link>
     </ScreenWrapper>
   );
--- app/Subscriptions/[id].tsx
+++ app/Subscriptions/[id].tsx
@@ -1,16 +1,17 @@
+import { ScreenWrapper } from "@/components/ScreenWrapper";
 import { Href, Link } from "expo-router";
 import { useLocalSearchParams } from "expo-router/build/hooks";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
 
 const SubscriptionDetails = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
 
   return (
-    <View>
+    <ScreenWrapper>
       <Text>Subscription Details: {id}</Text>
       <Link href={"/" as Href}>Go Back</Link>
-    </View>
+    </ScreenWrapper>
   );
 };
 
